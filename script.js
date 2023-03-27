00a907330a5a4c6ffd17f84d0a631af2b3c90d19
Jose Ovalle <ja.ovalle2@uniandes.edu.co>
2023-03-27 18:23:21 -0500

    Fixed mechanic of painting & Added default values (v2.0)

--- script.js
+++ script.js
@@ -2,39 +2,43 @@ const container_table = document.querySelector('.table');
 const totalSpaceOnGrid = 500*500;
 const elements = document.getElementsByClassName('.square')
 const slider = document.getElementById('slider');
+const labelslider = document.querySelector('.labelslider')
+slider.value = 24;
 
-
-
-
+createGrid(slider.value, totalSpaceOnGrid);
 
 slider.addEventListener('input', function() {
     createGrid(slider.value, totalSpaceOnGrid)
 });
 
-// for (let i = 1; i <= numberOfSquares; i++) {
-//     let min_width = Math.sqrt(totalSpaceOnGrid/numberOfSquares)
-//     let newGrid = document.createElement('div')
-//     newGrid.style.width = `${min_width}px`
-//     newGrid.classList.add('square')
-//     newGrid.addEventListener('mouseover', function() {
-//         newGrid.classList.add('whenhover')
-//     });
-//     container_table.appendChild(newGrid)
-// }
-
 function createGrid(value, totalSpaceOnGrid) {
     const numberOfSquares = value * value;
-    const min_width = Math.sqrt(totalSpaceOnGrid/numberOfSquares);
+    const min_width = Math.sqrt(totalSpaceOnGrid / numberOfSquares);
     container_table.innerHTML = "";
+    
+    let isDragging = false;
+  
     for (let i = 1; i <= numberOfSquares; i++) {
-        const newGrid = document.createElement('div')
-        newGrid.style.width = `${min_width}px`
-        newGrid.classList.add('square')
-        newGrid.addEventListener('mouseover', function() {
-            newGrid.classList.add('whenhover')
-        });
-        container_table.appendChild(newGrid)
+      const newGrid = document.createElement('div');
+      newGrid.style.width = `${min_width}px`;
+      newGrid.classList.add('square');
+      
+      newGrid.addEventListener('mousedown', function() {
+        isDragging = true;
+      });
+  
+      newGrid.addEventListener('mousemove', function() {
+        if (isDragging) {
+          newGrid.classList.add('whenhover');
+        }
+      });
+  
+      newGrid.addEventListener('mouseup', function() {
+        isDragging = false;
+      });
+  
+      container_table.appendChild(newGrid);
+      labelslider.textContent = `${slider.value}X${slider.value}`
     }
-
-}
-
+  }
+  
