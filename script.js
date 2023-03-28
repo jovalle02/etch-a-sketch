19416186481f23118087a51634833c52bea50ca4
Jose Ovalle <ja.ovalle2@uniandes.edu.co>
2023-03-28 07:39:02 -0500

    Organizing code and adding buttons & comments

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ const totalSpaceOnGrid = 500*500;
 const elements = document.getElementsByClassName('.square')
 const slider = document.getElementById('slider');
 const labelslider = document.querySelector('.labelslider')
-slider.value = 24;
+const btnremove = document.querySelector('.btnremove')
 
+//DEFAULT VALUES WHEN THE WEBSITE IS OPENED
+slider.value = 24;
 createGrid(slider.value, totalSpaceOnGrid);
 
+//WHEN THE SLIDER IS MOVED AROUND THIS FUNCTION TAKES ACTION AND UPDATES THE GRID
 slider.addEventListener('input', function() {
     createGrid(slider.value, totalSpaceOnGrid)
 });
 
+//FUNTION TO CLEAR THE GRID WHEN CLICKED
+btnremove.addEventListener('click', function() {
+    createGrid(slider.value, totalSpaceOnGrid)
+});
+
+//CREATES THE FUNCTION THAT MAKES THE GRID BASED ON THE VALUES OF THE SLIDER
 function createGrid(value, totalSpaceOnGrid) {
     const numberOfSquares = value * value;
     const min_width = Math.sqrt(totalSpaceOnGrid / numberOfSquares);
